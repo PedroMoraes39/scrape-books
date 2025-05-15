c8a462b3efdf44fdf9c690bc5c8d66960d7d38e9
PedroMoraes39 <phcmoraes39@gmail.com>
2025-05-15 17:55:48 -0300

    Add scraping of book details: stock, category, rating, and product page link

--- scrape_books.py
+++ scrape_books.py
@@ -3,19 +3,37 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
-base_url = 'http://books.toscrape.com/catalogue/page-{}.html'
+BASE_URL = 'http://books.toscrape.com/catalogue/page-{}.html'
+BOOK_BASE = 'http://books.toscrape.com/catalogue/{}'
 
 books_data = []
 page = 1
 
 os.makedirs('data', exist_ok=True)
 
+def get_rating(book):
+    
+    rating_classes = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5
+    }
+    star_tag = book.find('p', class_='star-rating')
+    
+    for class_name in star_tag.get('class', []):
+        
+        if class_name in rating_classes:
+            
+            return rating_classes[class_name]
+        
+    return None
+
 while True:
     
     print(f'Processando página {page}...')
-    
-    url = base_url.format(page)
-    response = requests.get(url)
+    response = requests.get(BASE_URL.format(page))
 
     if response.status_code != 200:
         
@@ -29,16 +47,30 @@ while True:
         
         title = book.h3.a['title']
         price = book.select_one('.price_color').text
+        rating = get_rating(book)
+        partial_link = book.h3.a['href']
+        book_link = BOOK_BASE.format(partial_link)
+
+        book_response = requests.get(book_link)
+        book_soup = BeautifulSoup(book_response.text, 'html.parser')
+
+        stock = book_soup.select_one('p.instock.availability').text.strip()
+
+        breadcrumb = book_soup.select('ul.breadcrumb li a')
+        category = breadcrumb[2].text.strip() if len(breadcrumb) > 2 else 'Desconhecida'
 
         books_data.append({
             'title': title,
-            'price': price
+            'price': price,
+            'rating': rating,
+            'stock': stock,
+            'category': category,
+            'link': book_link
         })
 
     page += 1
 
 df = pd.DataFrame(books_data)
-
 df.to_csv('data/books.csv', index=False, encoding='utf-8')
 
-print('Scraping finalizado. Arquivo salvo em data/books.csv')
+print('Scraping completo com dados adicionais salvos em data/books.csv')
