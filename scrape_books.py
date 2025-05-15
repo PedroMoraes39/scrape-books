e075a8ce96c531d16f9b31081be5f0a6a3ec78fa
PedroMoraes39 <phcmoraes39@gmail.com>
2025-05-15 10:36:55 -0300

    Refactor pagination loop to stop dynamically when no more pages are found

--- scrape_books.py
+++ scrape_books.py
@@ -1,21 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
-url = 'http://books.toscrape.com/'
+base_url = 'http://books.toscrape.com/catalogue/page-{}.html'
 
-response = requests.get(url)
+books_data = []
+page = 1
 
-if response.status_code == 200:
+os.makedirs('data', exist_ok=True)
+
+while True:
     
+    print(f'Processando página {page}...')
+    
+    url = base_url.format(page)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        
+        print('Não há mais páginas para processar.')
+        break
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    
     books = soup.select('article.product_pod')
 
     for book in books:
         
         title = book.h3.a['title']
         price = book.select_one('.price_color').text
-        print(f'Título: {title} | Preço: {price}')
-        
-else:
-    print(f'Erro ao acessar a página. Status code: {response.status_code}')
+
+        books_data.append({
+            'title': title,
+            'price': price
+        })
+
+    page += 1
+
+df = pd.DataFrame(books_data)
+
+df.to_csv('data/books.csv', index=False, encoding='utf-8')
+
+print('Scraping finalizado. Arquivo salvo em data/books.csv')
